39116864657a7755b8fa0e7a47801c2e3656ef32
Vedavyas <vedavyasjoshi121@gmail.com>
2024-09-10 16:32:55 +0530

    Adds keep-alive to prevent inactivity

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const fetch = require("cross-fetch");
+const http = require("http");
+const https = require("https");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+
+const protocol = process.env.URL.startsWith("https") ? https : http;
+
+setInterval(() => {
+  const url = process.env.URL || "http://localhost";
+  protocol
+    .get(url + "/keep-alive", (res) => {
+      console.log(`Keep-alive ping successful with code ${res.statusCode}`);
+    })
+    .on("error", (err) => {
+      console.error("Keep-alive ping failed:", err);
+    });
+}, 14 * 60 * 1000);
 
 app.get("/api/suggest", async (req, res) => {
   const { searchText } = req.query;
@@ -33,6 +49,10 @@ app.get("/api/suggest", async (req, res) => {
       console.error(error);
       res.status(500).send("An error occurred");
     });
+});
+
+app.get("/keep-alive", (req, res) => {
+  res.status(200).send("Server is alive!");
 });
 
 app.get("/", (req, res) => {
